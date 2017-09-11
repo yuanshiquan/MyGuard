b1489f69fe25652fabc8f1712ec96532d526fb81
yuanshiquan <1559536367@qq.com>
2017-09-11 15:30:17 +0800

    hbuyuyob

--- app/src/main/java/com/example/student/myguard/MainActivity.java
+++ app/src/main/java/com/example/student/myguard/MainActivity.java
@@ -9,5 +9,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_main);
+        if(1==1){
+            System.out.println("hello world!");
+            System.out.println("asdfsadf");
+     }
     }
 }
